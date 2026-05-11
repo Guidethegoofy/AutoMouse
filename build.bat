9a893cf36ce93e9b0049336ccd4d68abf24d11ab
Guidethegoofy <kitkzaza1@gmail.com>
2026-05-11 21:13:38 +0700

    build: update pyinstaller configuration to include all customtkinter assets

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Building AutoMouse Executable...
-pyinstaller --noconfirm --onefile --windowed --name "AutoMouse" --add-data "AutoMouse.py;." AutoMouse.py
+pyinstaller --noconfirm --onefile --windowed --name "AutoMouse" --collect-all customtkinter AutoMouse.py
 echo Build complete! Check the 'dist' folder.
 pause
